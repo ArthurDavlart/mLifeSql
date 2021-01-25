821d119a66e63e12499be092519d33061723d7c7
ArthurDavlart <arthur.davletkaliyev@mail.ru>
2021-01-25 09:18:51 +0400

    Отрефакторил запрос, который показовал по дням количество упатребленных калорий

--- show_daily_calories_from_all_time.sql
+++ show_daily_calories_from_all_time.sql
@@ -1,10 +1,7 @@
--- Вывести дневное количество калорий за все время
+-- Вывести дневное количество калорий за все время 
 
-SELECT t.recording_timestamp:: DATE, SUM(t.sum) 
-FROM (SELECT f.recording_timestamp, SUM(0.01 * f.amount * p.kcal)
-	FROM food_diary as f
-	JOIN products as p ON f.product_id = p.id
-	GROUP BY f.recording_timestamp
-	ORDER BY f.recording_timestamp ) as t
-GROUP BY t.recording_timestamp :: date
-ORDER BY t.recording_timestamp :: date;
+SELECT f.recording_timestamp:: DATE, SUM(0.01 * p.kcal * f.amount) 
+FROM products p 
+JOIN food_diary f ON f.product_id = p.id 
+GROUP BY f.recording_timestamp::DATE
+ORDER BY 1;
