a29bbd3ef939ec7b0554fc5b97ce322ab6b2ae31
ArthurDavlart <arthur.davletkaliyev@mail.ru>
2021-01-26 14:53:18 +0400

    Вынес подзапрос в структуру WITH...AS

--- display_the_ name_of_all_products_and_the_number_of_times_the_product_was_consumed_during_the_day.sql
+++ display_the_ name_of_all_products_and_the_number_of_times_the_product_was_consumed_during_the_day.sql
@@ -1,9 +1,12 @@
 -- Вывести имя всех продуктов и количество раз, когда продукт был употреблен в течении дня
 
+WITH today_food_diary AS (
+	SELECT * 
+	FROM food_diary 
+	WHERE recording_timestamp BETWEEN 'today':: DATE AND 'tomorrow':: DATE
+)
 SELECT p.name, COUNT(f.product_id) 
 FROM products p 
-LEFT OUTER JOIN(SELECT * 
-				FROM food_diary 
-				WHERE recording_timestamp BETWEEN 'today':: DATE AND 'tomorrow':: DATE) AS f ON f.product_id = p.id 
+LEFT OUTER JOIN today_food_diary AS f ON f.product_id = p.id 
 GROUP BY 1 
 ORDER BY 2;
