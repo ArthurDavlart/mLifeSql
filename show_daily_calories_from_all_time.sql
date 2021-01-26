d3ee8ba8be10341017ea2a3b689081ad281b136f
ArthurDavlart <arthur.davletkaliyev@mail.ru>
2021-01-26 14:52:25 +0400

    Мелкий рефакторинг

--- show_daily_calories_from_all_time.sql
+++ show_daily_calories_from_all_time.sql
@@ -3,5 +3,5 @@
 SELECT f.recording_timestamp:: DATE, SUM(0.01 * p.kcal * f.amount) 
 FROM products p 
 JOIN food_diary f ON f.product_id = p.id 
-GROUP BY f.recording_timestamp::DATE
+GROUP BY 1
 ORDER BY 1;
